2ddaa947d4fe01bce4af9dea71064b113a96443d
Ed Bacayo <edbacayo@yahoo.com>
2021-02-19 22:43:22 +0800

    Add video portfolio

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+
+const port = 3000;
+
+app.listen(process.env.PORT || port, function() {
+    console.log("Listening...");
+})
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/video.html");
+})
